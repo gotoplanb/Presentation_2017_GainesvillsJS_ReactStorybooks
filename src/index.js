22a126aacce32bff26c6eee5b4aa85ae807df39c
Dave Stanton <davestanton.us@gmail.com>
2017-02-21 15:53:38 -0500

    react-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import { Router, Route, browserHistory } from 'react-router';
 import './styles/salesforce-lightning-design-system.min.css'
 import './index.css';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
 
-ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={Home}/>
+    <Route path="*" component={NotFound}/>
+  </Router>
+), document.getElementById('root'));
