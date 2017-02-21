a59194dc31b5eaedf6739922e2148f39643cea09
Dave Stanton <davestanton.us@gmail.com>
2017-02-21 15:06:13 -0500

    Add Lightning Design System.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './styles/salesforce-lightning-design-system.min.css'
 import './index.css';
 
 ReactDOM.render(
